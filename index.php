79e70db7dc7435dbc2c371b907879c8d2e13322c
ceclun <s151302@student.hb.se>
2017-03-07 15:09:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+<title>J & C Blogg</title>
+<meta charset="UTF-8">
+<meta http-equiv="Content-Type" content="text/html"/>
+
+<!-- Style sheets -->
+<link href="style.css" rel="stylesheet" type="text/css" >
+ 
+</head>
+ 
 <?php
 date_default_timezone_set('Europe/Stockholm');
 include 'common.library.php';
