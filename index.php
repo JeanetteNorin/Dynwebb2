6a0c7b711d5920c23b5d7299bee97c850309e67c
ceclun <s151302@student.hb.se>
2017-03-08 19:12:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<title>Blogg</title>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+</head>
+<body>
 
+
+<?php
 date_default_timezone_set('Europe/Stockholm');
 include 'common.library.php';
 include 'header.php';
+db_connect();
 
+$query = "SELECT date_time, text, title, catid FROM blog
+  ORDER BY date_time DESC";
 
-
- ?>
-
-<body>
-<?php
-if (isset($_SESSION['uid'])) {
-  echo $_SESSION['uid'];
-} else {
-  echo "you are not logged in";
+$result = mysql_query($query);
+while (list($date, $text, $title, $catid) = mysql_fetch_row($result)) {
+	$query2 = "SELECT category FROM cat WHERE catid='$catid'";
+	$result2 = mysql_query($query2);
+	while ($row2 = mysql_fetch_array($result2)) {
+		$cat = $row2['category'];
+	}
+	echo "<h2>$title</h2>";
+	echo "<p>$text</p>";
+	echo "<p>@ $date, kategori: $cat</p>";
+	
 }
- ?>
+ 
+?>
+
 </body>
-
 </html>
-
