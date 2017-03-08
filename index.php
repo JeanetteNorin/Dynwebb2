54ebbd31851bb2312f0550a877b20a3aa05191c3
queenbumblebee <queenbumblebee@users.noreply.github.com>
2017-03-08 14:13:29 +0100

    Update index.php
    Flyttar header till en egen fil

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
+
 date_default_timezone_set('Europe/Stockholm');
-include 'dbh.php';
-include 'comments.inc.php';
+include 'common.library.php';
 include 'header.php';
 
 
@@ -9,24 +9,13 @@ include 'header.php';
  ?>
 
 <body>
-  <iframe width="420" height="315"
-  src="https://www.youtube.com/embed/XGSy3_Czz8k">
-  </iframe>
-
 <?php
-//Gets new comment from input and insert them into db
-echo  "<form method='POST' action='".setComments($conn)."'>
-        <input type='hidden' name='uid' value='Anonymous'>
-        <input type='hidden' name='date' value='".date('Y-m-d H:i:s')."'>
-        <textarea name='message'></textarea><br>
-        <button type='submit' name='commentSubmit'>Comment</button>
-      </form>";
-
-//Call function to get comments from db
-getComments($conn);
-
+if (isset($_SESSION['uid'])) {
+  echo $_SESSION['uid'];
+} else {
+  echo "you are not logged in";
+}
  ?>
-
 </body>
 
 </html>
