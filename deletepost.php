7c832f48cb1083091450667937ab1dc392537840
queenbumblebee <queenbumblebee@users.noreply.github.com>
2017-03-09 18:10:58 +0100

    Create deletepost.php

---
+++ deletepost.php
@@ -0,0 +1,31 @@
+<?php
+date_default_timezone_set('Europe/Stockholm');
+include 'common.library.php';
+include 'header.php';
+//db_connect();
+
+if (isset($_GET['id'])) {
+  $id = $_GET['id'];
+  $query = "DELETE FROM blog WHERE id='$id'";
+  $result = mysqli_query($conn, $query);
+  if ((mysql_affected_rows() == 0) || (mysql_affected_rows() == -1)) {
+    echo "Borttaget!";
+  }
+  else {
+    echo "Ej Borttaget!";
+  }
+}
+
+$query = "SELECT date_time, title, text, id FROM blog ORDER BY title DESC";
+
+$result = mysqli_query($conn, $query);
+while (list($date, $title, $text, $id) = mysqli_fetch_row($result)) {
+  echo "<a href=\"deletepost.php?id=$id\">$title</a>";
+  echo "<p>$text</p>";
+  echo "<p>$date</p>";
+}
+
+?>
+
+</body>
+</html>
