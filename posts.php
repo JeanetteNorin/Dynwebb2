621055b74f74ddb420d822072ac44b35e8f98a58
ceclun <s151302@student.hb.se>
2017-03-09 17:08:45 +0100

    Update posts.php

--- posts.php
+++ posts.php
@@ -3,56 +3,59 @@ session_start();
 if(!isset($_SESSION['uid'])){
    header("Location:index.php");
 }
+
 date_default_timezone_set('Europe/Stockholm');
 include 'common.library.php';
-db_connect();
+//db_connect();
 include 'header.php';
 
  
-/* finns det ett värde på superglobalen med namn
-submit (har formuläret skickats?)? */
+//check to see if form has been sent
 if (isset($_POST['submit'])) {
  
   $title = trim_string($_POST['title']);
   $text = trim_string($_POST['text']);
-  /* bygg ett datum enligt YYYY-MM-DD HH:II:SS
-  se date()-funktionen på php.net för mer info */
+  
+  //building date 
   $date = date("Y-m-d H:i:s");
-  $catid = $_POST['category'];	
-	
+  
+  $categoryid = $_POST['category'];	
   $query = "INSERT INTO blog SET title='$title', text='$text',
-    date_time='$date', catid='$catid'";
+    date_time='$date', catid='$categoryid'";
 		
-  $result = mysql_query($query);
+  $result = mysqli_query($conn, $query);
 		
   if (!$result) {
-    $message  = 'Invalid query: ' . mysql_error() . "\n";
+    $message  = 'Invalid query: ' . mysqli_error() . "\n";
     $message .= 'Whole query: ' . $query;
     die($message);
   }  		
   else {
-    echo "<p>Skrivningen med query $query till databasen lyckades!</p>";
+    echo "<p>Post published!</p>";
   } 		
  
 }	
- 
 ?>
+
   <form method="post" action="posts.php">
     <p>Title<br />
     <input type="text" name="title" size="50" /></p>
     <p>Text<br />
     <textarea name="text" cols="60" rows="20"></textarea></p>
     <p>Category:<br />
-<?php
-$query = "SELECT catid, category FROM cat ORDER BY category";
-$result = mysql_query($query);
-while($row = mysql_fetch_array($result)) {
-  $value = $row["catid"];
-  $name = $row["category"];
-  $pairs["$value"] = $name;
-}
-echo create_dropdown("category",$pairs,"Choose from category list:",1,"");
-?>
+
+	<?php
+	//create menu ffor category choice
+	$query = "SELECT catid, category FROM cat ORDER BY category";
+	$result = mysqli_query($conn, $query);
+	while($row = mysqli_fetch_array($result)) {
+		$value = $row["catid"];
+		$name = $row["category"];
+		$pairs["$value"] = $name;
+	}
+	echo create_dropdown("category",$pairs,"Choose from category list:",1,"");
+	?>
+	
     </p>    
     <p><input type="submit" name="submit" value="Publish!" /></p>
   </form>			
